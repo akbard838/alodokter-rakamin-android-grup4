f71f33d533943b8e262b7ca81eef128c833dd142
Evandio <evandiomb@gmail.com>
2021-12-02 22:14:27 +0700

    Basic Layout for ProfileActivity.kt

---
+++ app/src/main/java/com/example/androidgroup4/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidgroup4.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidgroup4.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
