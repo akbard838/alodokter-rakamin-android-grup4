6d61e5c6191d4737bb67bdd1149539f2fb198298
Evandio <evandiomb@gmail.com>
2021-12-03 13:35:41 +0700

    Update ProfileActivity.kt

--- app/src/main/java/com/example/androidgroup4/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/androidgroup4/ui/profile/ProfileActivity.kt
@@ -1,12 +1,56 @@
 package com.example.androidgroup4.ui.profile
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.viewbinding.ViewBinding
 import com.example.androidgroup4.R
+import com.example.androidgroup4.base.BaseActivity
+import com.example.androidgroup4.databinding.ActivityLoginBinding
+import com.example.androidgroup4.databinding.ActivityProfileBinding
 
-class ProfileActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+class ProfileActivity : BaseActivity<ActivityProfileBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ViewBinding = ActivityLoginBinding::inflate
+
+    override fun initIntent() {
+
+    }
+
+    override fun initUI() {
+        setupToolbar(binding.toolbarMain.toolbar, true, getString(R.string.label_login))
+    }
+
+    override fun initAction() {
+        binding.apply {
+            changePhoto.setOnClickListener {
+
+            }
+            editProfile.setOnClickListener {
+
+            }
+            changePassword.setOnClickListener {
+
+            }
+            logout.setOnClickListener {
+
+            }
+        }
+    }
+
+    override fun initProcess() {
+
+    }
+
+    override fun initObservable() {
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return true
     }
 }
