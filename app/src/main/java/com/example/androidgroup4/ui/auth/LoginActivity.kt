27179c21192aa2702423c34ed8e019e9476efba0
akbard838 <akbard838@gmail.com>
2021-12-10 06:39:41 +0700

    implement(login): implement mock login API

--- app/src/main/java/com/example/androidgroup4/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/androidgroup4/ui/auth/LoginActivity.kt
@@ -4,17 +4,22 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.example.androidgroup4.R
 import com.example.androidgroup4.base.BaseActivity
+import com.example.androidgroup4.data.source.remote.network.ApiResponse
+import com.example.androidgroup4.data.source.remote.response.UserResponse
 import com.example.androidgroup4.databinding.ActivityLoginBinding
+import com.example.androidgroup4.ui.UserViewModel
 import com.example.androidgroup4.ui.main.MainActivity
+import com.example.androidgroup4.utils.*
 import com.example.androidgroup4.utils.constant.PreferenceKeys.IS_LOGIN
-import com.example.androidgroup4.utils.getAppPreferenceEditor
-import com.example.androidgroup4.utils.isFormValid
-import com.example.androidgroup4.utils.validateEmail
-import com.example.androidgroup4.utils.validateNonEmpty
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
     companion object {
@@ -24,6 +29,8 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
             }
         }
     }
+
+    private val userViewModel: UserViewModel by viewModels()
 
     override val bindingInflater: (LayoutInflater) -> ViewBinding = ActivityLoginBinding::inflate
 
@@ -47,9 +54,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
                 tilPassword.validateNonEmpty()
 
                 isFormValid(listOf(tilEmail, tilPassword)) {
-                    getAppPreferenceEditor(this@LoginActivity).putBoolean(IS_LOGIN, true).commit()
-                    MainActivity.start(this@LoginActivity)
-                    finish()
+                    postLogin()
                 }
             }
 
@@ -74,6 +79,55 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
             }
         }
         return true
+    }
+
+    private fun postLogin() {
+        lifecycleScope.launchWhenStarted {
+            userViewModel.postLogin(
+                binding.edtEmail.text.toString(),
+                binding.edtPassword.text.toString()
+            ).collect {
+                when (it) {
+                    is ApiResponse.Success -> {
+                        hideLoading()
+                        if (isEmailAndPasswordVerified(it.data)){
+                            getAppPreferenceEditor(this@LoginActivity).putBoolean(IS_LOGIN, true).commit()
+                            MainActivity.start(this@LoginActivity)
+                            finish()
+                        }
+                    }
+                    is ApiResponse.Failure -> {
+                        hideLoading()
+                        showToast(this@LoginActivity, it.message)
+                    }
+                    is ApiResponse.Loading -> {
+                        showLoading()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun isEmailAndPasswordVerified(user: List<UserResponse>?): Boolean {
+        return if (user.isNullOrEmpty()) {
+            binding.tilEmail.error = "Email tidak terdaftar"
+            false
+        } else {
+            binding.tilEmail.isErrorEnabled = false
+            if (user[0].email != binding.edtEmail.text.toString()) {
+                binding.tilEmail.error = "Email tidak terdaftar"
+                false
+            } else {
+                binding.tilEmail.isErrorEnabled = false
+                if (user[0].password != binding.edtPassword.text.toString()) {
+                    binding.tilPassword.error = "Password tidak sesuai"
+                    false
+                } else {
+                    binding.tilPassword.isErrorEnabled = false
+                    true
+                }
+            }
+        }
     }
 
     private fun initDummyData() {
