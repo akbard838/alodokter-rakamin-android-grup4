4010917620d7333e823419908f4de55be90967eb
alfian <alfianhacker@gmail.com>
2021-12-17 15:00:10 +0800

    implement(list-doctors): implement list doctor api.

--- app/src/main/java/com/example/androidgroup4/data/doctor/remote/DoctorApiService.kt
+++ app/src/main/java/com/example/androidgroup4/data/doctor/remote/DoctorApiService.kt
@@ -1,13 +1,20 @@
 package com.example.androidgroup4.data.doctor.remote
 
 import com.example.androidgroup4.base.BaseApiResponse
+import com.example.androidgroup4.data.article.model.ArticleResponse
 import com.example.androidgroup4.data.doctor.model.DoctorResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface DoctorApiService {
 
     @GET("doctors")
     suspend fun getDoctors() : Response<BaseApiResponse<List<DoctorResponse>>>
+
+    @GET("doctors/search")
+    suspend fun getSearchDoctors(
+        @Query("fullname") fullName: String
+    ) : Response<BaseApiResponse<List<DoctorResponse>>>
 
 }
